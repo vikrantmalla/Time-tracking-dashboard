21f93d84c0124f48e86d626151c2ffb6825b8aa1
Vikrant Malla <vikrantmalla999@gmail.com>
2022-01-10 17:08:09 +0545

    Revert "integrate Json-Server"
    This reverts commit 65ad3303aa101b24e8bc1c9c84339040878c179e.

--- src/component/Dashboard.jsx
+++ src/component/Dashboard.jsx
@@ -1,19 +1,7 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
+import data from '../API/data'
 
 const Dashboard = () => {
-    const [data, setdata] = useState([]);
-
-    useEffect(() => {
-        fetch('http://localhost:8000/data')
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                // console.log(data);
-                setdata(data)
-            })
-    }, [])
-
     const [timePeriod, setTimePeriod] = useState("weekly");
     const [previousMessage, setPreviousMessage] = useState("Last Week");
 
@@ -27,44 +15,43 @@ const Dashboard = () => {
         <>
             <article className="grid">
 
-                <header className="header">
-                    <div className="user-hero">
-                        <img src="./images/image-jeremy.png" alt="im" />
-                        <div className="user-info">
-                            <p>Report for</p>
-                            <h2>Jeremy Robson</h2>
+                    <header className="header">
+                        <div className="user-hero">
+                            <img src="./images/image-jeremy.png" alt="im" />
+                            <div className="user-info">
+                                <p>Report for</p>
+                                <h2>Jeremy Robson</h2>
+                            </div>
                         </div>
-                    </div>
-                    <div className="time-options">
-                        <button onClick={periodChangeHandler} className={timePeriod === "daily" ? "showing" : ""} name="daily" type="button">
-                            Daily
-                        </button>
-                        <button onClick={periodChangeHandler} className={timePeriod === "weekly" ? "showing" : ""} name="weekly" type="button" >
-                            Weekly
-                        </button>
-                        <button onClick={periodChangeHandler} className={timePeriod === "monthly" ? "showing" : ""} name="monthly" type="button" >
-                            Monthly
-                        </button>
-                    </div>
-                </header>
-                {
-                    data.map((activity, id) => {
-                        const { title, timeframes } = activity;
-                        return (
-                            <article className={"card " + title} key={id}>
-                                <div className="card-head">
+                        <div className="time-options">
+                            <button onClick={periodChangeHandler} className={timePeriod === "daily" ? "showing" : ""} name="daily" type="button">
+                                Daily
+                            </button>
+                            <button onClick={periodChangeHandler} className={timePeriod === "weekly" ? "showing" : ""} name="weekly" type="button" >
+                                Weekly
+                            </button>
+                            <button onClick={periodChangeHandler} className={timePeriod === "monthly" ? "showing" : ""} name="monthly" type="button" >
+                                Monthly
+                            </button>
+                        </div>
+                    </header>
+                    {
+                        data.map((activity, id) => {
+                            const { title, timeframes } = activity;
+                            return (
+                                <article className={"card " + title} key={id}>
+                                    <div className="card-head">
                                     <h2 className="card-title">{title}</h2>
                                     <img src="./images/icon-ellipsis.svg" alt="ellipsis" />
-                                </div>
-                                <div className="card-info">
+                                    </div>
+                                    <div className="card-info">
                                     <h2 className="current-hours">{timeframes[timePeriod].current} hrs</h2>
                                     <p className="previous-hours">{previousMessage} - {timeframes[timePeriod].previous} hrs</p>
-                                </div>
-                            </article>
-                        )
-                    })
-                }
-
+                                    </div>
+                                </article>
+                            )
+                        })
+                    }
 
             </article>
         </>
